49430bcf3526ae3ad2595896f8556a033c1a33be
tunveyyy <panditanvi@gmail.com>
2020-03-24 00:36:16 -0500

    React function based components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Person from './Person/Person';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = props => {
+  const [personState, setPersonState] = useState({
+    person: [
+      { name: "Max", age: 10 },
+      { name: "Molly", age:12 },
+      { name: "Bruno", age:5}
+    ],
+    otherState: 'Some other value'
+  })
 
+  const switchNameHandler = () => {
+    setPersonState({ person: [
+      { name: "Maximilian", age: 10 },
+      { name: "Molly", age:12 },
+      { name: "Qwerty", age:25}
+    ]
+
+  } )
+  }
+  
+    return (
+      <div className = "App">
+        <h1>  Hi, I am a react App</h1>
+        <h2> I am being coded by a Noob ;)</h2>
+        <button onClick= { switchNameHandler }>Switch Name</button>      
+        <Person name = {personState.person[0].name} age = {personState.person[0].age} click = {switchNameHandler}/>
+        <Person name = {personState.person[1].name} age = {personState.person[1].age}>My Hobbies: farting </Person>
+        <Person name = {personState.person[2].name} age = {personState.person[2].age}/>
+      </div>
+    )
+}
 export default App;
+
+
